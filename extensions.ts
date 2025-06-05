917f52f77ab65923eb5e9ffdca899fb608771d3b
twahir21 <twahirsudy3@gmail.com>
2025-06-05 20:41:24 +0300

    adding fira code for better experience

---
+++ extensions.ts
@@ -0,0 +1,29 @@
+/**
+ * ? something
+ * TODO: this is for to do
+ * * powered by better comment
+ * ! do not use 
+ * @author Twahir Sudy
+ * use "@" symbol for more .... 
+ */
+
+// ! this is an alert
+// * here we go
+// ? something good is comming ....
+
+// i also have md to pdf converter called markdown pdf converter it has more
+// code time for checking how much time spent in coding ...
+
+// added windsurf ai completion without limit like capliot and use ctrl + shift + A
+// alt +c for new chat
+// ctrl + I for inline code 
+
+
+// google font for auto import in vs code use command pallete search google fonts and select ur font family easier
+
+// import cost for showing since of libs imported for max performance and speed
+
+// i change the theme to use Tokyo Night and Catppuccin themes and One Dark Pro 
+
+// installing fira code font for better experience
+// use sudo apt install fonts-firacode then in settings under fonts use 'Fira Code' and restart vscode
